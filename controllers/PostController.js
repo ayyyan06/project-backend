2183f8111705ddc7083d3dc3595ec06c64ad4c22
ayyyan06 <ayan_kunirbaev@mail.ru>
2025-01-10 16:04:37 +0500

    first commit

---
+++ controllers/PostController.js
@@ -0,0 +1,115 @@
+import PostModel from '../models/Post.js'
+
+export const getAll = async(req, res) => {
+    try {
+        const posts = await PostModel.find().populate('user').exec()
+
+        res.json(posts)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: "Cannot get posts"
+        })
+    }
+}
+
+export const getOne = async (req, res) => {
+    try {
+        const postId = req.params.id
+
+        PostModel.findOneAndUpdate(
+            { 
+                _id: postId
+            },
+            {
+                $inc: { viewsCount: 1 }
+            },
+            {
+                returnDocument: "After" 
+            }  
+        )
+        .then(doc => {
+            if (!doc) {
+                throw Error;
+            }
+            res.json(doc);
+        })
+        .catch(err => res.status(404).json({ message: 'Article not found'}));
+
+    } catch (err) {
+        
+    }
+}
+
+export const create = async (req, res) => {
+    try {
+      const doc = new PostModel({
+        title: req.body.title,
+        text: req.body.text,
+        imageUrl: req.body.imageUrl,
+        tags: req.body.tags,
+        user: req.userId,
+      });
+  
+      const post = await doc.save();
+  
+      res.json(post);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({
+        message: 'Cannot create a post',
+      });
+    }
+};
+
+export const update = async (req, res) => {
+    try {
+        const postId = req.params.id
+
+    await PostModel.updateOne(
+        {
+            _id: postId
+        },
+        {
+            title: req.body.title,
+            text:req.body.text,
+            imageUrl: req.body.imageUrl,
+            tags: req.body.tags,
+            user: req.body.user,
+        }
+    )
+
+    res.json({
+        success:true
+    })
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json({
+            message: "Npt found"
+        })
+    }
+}
+
+export const remove = async(req, res) => {
+    try {
+        const postId = req.params.id
+
+        const doc = await PostModel.findOneAndDelete({ _id: postId });
+    
+        if (!doc) {
+            return res.status(404).json({
+                message: 'Статья не найдена',
+            });
+        }
+    
+        res.json({
+            success: true,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Cannot remove post',
+        });
+    }
+    
+}
---
+++ index.js
@@ -0,0 +1,47 @@
+import express from "express"
+import mongoose from 'mongoose'
+
+import { registerValidation, loginValidation, postCreateValidation } from './validations.js'
+
+import checkAuth from "./utils/checkAuth.js"
+
+import * as UserController from './controllers/UserController.js'
+import * as PostController from './controllers/PostController.js'
+
+mongoose.connect
+    (
+        'mongodb+srv://ayankunirbaev:wwwwww@cluster0.n1cmf.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0'
+    ) 
+    .then(() => console.log('DB is ok'))
+    .catch(() => console.log('DB err', err))
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {  
+    res.send("Hello world")
+})
+
+app.post('/auth/login', loginValidation, UserController.login)
+
+app.post('/auth/register', registerValidation, UserController.register)
+
+app.post('/posts', checkAuth, postCreateValidation, PostController.create)
+
+app.get('/posts', PostController.getAll)
+
+app.get('/posts/:id', PostController.getOne)
+
+app.patch('/posts/:id', checkAuth, PostController.update)
+
+app.delete('/posts/:id', checkAuth,  PostController.remove)
+
+app.get('/auth/me', checkAuth, UserController.getMe)
+
+app.listen(4444, (err) => {
+    if(err) {
+        return console.log(err)
+    }
+
+    console.log('server is Ok')
+})
