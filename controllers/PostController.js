d9a972e8b8c7ab6045bad53a21db8dcd2b79e204
ayyyan06 <ayan_kunirbaev@mail.ru>
2025-02-21 18:44:00 +0500

    first commit

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,5 +1,24 @@
 import PostModel from '../models/Post.js'
 
+export const getLastTags = async (req, res) => {
+    try {
+        const posts = await PostModel.find().limit(5).exec()
+
+        const tags = posts
+            .map(obj => obj.tags)
+            .flat()
+            .slice(0, 5)
+            
+        console.log(tags)
+        res.json(tags)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: "Cannot get tags"
+        })
+    } 
+}
+
 export const getAll = async(req, res) => {
     try {
         const posts = await PostModel.find().populate('user').exec()
@@ -28,6 +47,7 @@ export const getOne = async (req, res) => {
                 returnDocument: "After" 
             }  
         )
+        .populate('user')
         .then(doc => {
             if (!doc) {
                 throw Error;
@@ -47,7 +67,7 @@ export const create = async (req, res) => {
         title: req.body.title,
         text: req.body.text,
         imageUrl: req.body.imageUrl,
-        tags: req.body.tags,
+        tags: req.body.tags.split(','),
         user: req.userId,
       });
   
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import express from "express"
 import mongoose from 'mongoose'
+import multer from 'multer'
+import cors from 'cors'
 
 import { registerValidation, loginValidation, postCreateValidation } from './validations.js'
 
-import checkAuth from "./utils/checkAuth.js"
-
-import * as UserController from './controllers/UserController.js'
-import * as PostController from './controllers/PostController.js'
+import {UserController, PostController} from './controllers/index.js'
+import {handleValidationErrors, checkAuth} from "./utils/index.js"
 
 mongoose.connect
     (
@@ -14,34 +14,57 @@ mongoose.connect
     ) 
     .then(() => console.log('DB is ok'))
     .catch(() => console.log('DB err', err))
+
 const app = express()
 
+const storage = multer.diskStorage({
+    destination: (_, __, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (_, file, cb) => {
+        cb(null, file.originalname)
+    },
+})
+
+const upload = multer({ storage })
+
 app.use(express.json())
+app.use(cors({
+    origin: 'http://localhost:3000'
+}))
+
+app.post('/auth/login', handleValidationErrors, loginValidation, UserController.login)
+app.post('/auth/register', registerValidation, handleValidationErrors,  UserController.register)
+// app.post('/register', handleValidationErrors, registerValidation, UserController.register)
+
+app.get('/auth/me', checkAuth, UserController.getMe)
+
+app.post('/upload', checkAuth, upload.single('image'), (req,res) => {
+    res.json({
+        url: ('/uploads/'+req.file.originalname)
+    })
+})
+
+// app.use(cors())
+
+app.use('/uploads', express.static('uploads'))
 
 app.get('/', (req, res) => {  
     res.send("Hello world")
 })
 
-app.post('/auth/login', loginValidation, UserController.login)
-
-app.post('/auth/register', registerValidation, UserController.register)
-
-app.post('/posts', checkAuth, postCreateValidation, PostController.create)
-
+app.get('/tags', PostController.getLastTags)
 app.get('/posts', PostController.getAll)
-
 app.get('/posts/:id', PostController.getOne)
-
-app.patch('/posts/:id', checkAuth, PostController.update)
-
+app.get('/posts/tags', PostController.getLastTags)
+app.post('/posts', checkAuth, postCreateValidation, PostController.create)
 app.delete('/posts/:id', checkAuth,  PostController.remove)
-
-app.get('/auth/me', checkAuth, UserController.getMe)
+app.patch('/posts/:id', checkAuth, postCreateValidation, handleValidationErrors, PostController.update)
 
 app.listen(4444, (err) => {
     if(err) {
         return console.log(err)
     }
-
+    
     console.log('server is Ok')
 })
