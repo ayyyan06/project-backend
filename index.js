2a0ae87f58f62eef5fb89d1f4bcb428450cdfa01
ayyyan06 <ayan_kunirbaev@mail.ru>
2025-02-25 13:33:26 +0500

    fifth commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import {UserController, PostController} from './controllers/index.js'
 import {handleValidationErrors, checkAuth} from "./utils/index.js"
 
 mongoose.connect
-    (process.env.MONGODB_URI) 
+    ('mongodb+srv://ayankunirbaev:wwwwww@cluster0.n1cmf.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0') 
     .then(() => console.log('DB is ok'))
     .catch((err) => console.log('DB err', err))
 
